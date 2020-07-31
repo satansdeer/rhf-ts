e5fc6860012b8d3dc2ec6c31bfd9bf13c6812ba8
Maksim Ivanov <satansdeer@gmail.com>
2020-07-31 14:01:39 +0200

    Add code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { useForm } from 'react-hook-form'
+import "./App.css";
+
+type Profile = {
+  firstname: string
+  lastname: string
+  age: number
+}
 
 function App() {
+  const {register, handleSubmit, errors} = useForm<Profile>()
+
+  const onSubmit = handleSubmit((data) => {
+    alert(JSON.stringify(data))
+  })
+
   return (
-    <form>
+    <main>
+    <form onSubmit={onSubmit}>
+      <div>
+        <label htmlFor="firstname">First Name</label>
+        <input ref={register({ required: true })} id="firstname" name="firstname" type="text"/>
+        {
+          errors.firstname && <div className="error">Enter your name</div>
+        }
+      </div>
+      <div>
+        <label htmlFor="lastname">Last Name</label>
+        <input ref={register({ required: true })} id="lastname" name="lastname" type="text"/>
+        {
+          errors.lastname && <div className="error">Enter your last name</div>
+        }
+      </div>
+      <div>
+        <label htmlFor="age">Age</label>
+        <input ref={register({ required: true })} id="age" name="age" type="text"/>
+        {
+          errors.age && <div className="error">Enter your age</div>
+        }
+      </div>
+      <button type="submit">Save</button>
     </form>
+    </main>
   );
 }
 
